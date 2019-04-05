67d540a523a6b73b583abd7926384b6c6c364dd8
Pierre Troger <ptroger@macbook-pro-de-pierre.home>
2019-04-05 14:48:28 +0200

    Leak

---
+++ get_next_line.c
@@ -0,0 +1,65 @@
+#include "get_next_line.h"
+#include <unistd.h>
+#include <stdio.h>
+#include "libft/libft.h"
+#include <fcntl.h>
+
+int			ft_copyindex(char **dst, char *src, char c)
+{
+	int		i;
+	int		count;
+	int		pos;
+
+	i = -1;
+	count = 0;
+	while (src[++i] && src[i] != c)
+	pos = i;
+	ft_strncpy(*dst, src, i);
+	return (pos);
+}
+
+static t_list			*get_correct_file(t_list **file, int fd)
+{
+	t_list				*tmp;
+
+	tmp = *file;
+	while (tmp)
+	{
+		if ((int)tmp->content_size == fd)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	tmp = ft_lstnew("\0", fd);
+	ft_lstadd(file, tmp);
+	tmp = *file;
+	return (tmp);
+}
+
+int						get_next_line(const int fd, char **line)
+{
+	char				*buf;
+	static t_list		*file;
+	int					i;
+	int					ret;
+	t_list				*curr;
+
+	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
+		return (-1);
+	curr = get_correct_file(&file, fd);
+	MALLCHECK((*line = ft_strnew(1)));
+	while ((ret = read(fd, buf, BUFF_SIZE)))
+	{
+		buf[ret] = '\0';
+		MALLCHECK((curr->content = ft_strjoin(curr->content, buf)));
+		free(buf);
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	if (ret < BUFF_SIZE && !ft_strlen(curr->content))
+		return (0);
+	i = ft_copyindex(line, curr->content, '\n');
+	(i < (int)ft_strlen(curr->content))
+		? curr->content += (i + 1)
+		: ft_strclr(curr->content);
+	return (1);
+}
